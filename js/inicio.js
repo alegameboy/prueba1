91e01dd45c23a995d798465664d090c63c93a2e5
alegameboy <65217985+alegameboy@users.noreply.github.com>
2020-05-25 17:28:46 -0600

    Add files via upload

---
+++ js/inicio.js
@@ -0,0 +1,20 @@
+function abrirLogin(){
+    document.getElementById("loginpop").style.display="block";
+}
+
+
+function cerrar(){
+    document.getElementById("loginpop").style.display="none";
+}
+
+function cerrar2(){
+    document.getElementById("loginpop").style.display="none";
+    document.getElementById("divL").style.display="block";
+    document.getElementById("divR").style.display="none";
+}
+
+
+function cambiodeLaR(){
+    document.getElementById("divL").style.display="none";
+    document.getElementById("divR").style.display="block";
+}
